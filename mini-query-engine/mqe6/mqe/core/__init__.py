36c127004bacf3909eb42eb2dea18b99ea29608e
Nikolai Potapov <rnjv2015@yandex.ru>
2026-02-01 19:08:55 +0100

    Final preparation for publication MQE6

--- mini-query-engine/mqe6/mqe/core/__init__.py
+++ mini-query-engine/mqe6/mqe/core/__init__.py
@@ -1,8 +1,27 @@
+from typing import Any, Optional
+
 from .context import ExecutionContext
 from .datatypes import ArrowColumn
 from .frames import DataFrame, LazyFrame
 from .logical_expr import col
 from .tables import DataBatch, SchemaField, TableSchema
+
+_default_ctx = ExecutionContext()
+
+
+def get_context() -> ExecutionContext:
+    return _default_ctx
+
+
+def from_dict(data: dict[str, list[Any]]) -> LazyFrame:
+    return _default_ctx.from_dict(data)
+
+
+def from_batches(
+    batches: list[DataBatch], schema: Optional[TableSchema] = None
+) -> LazyFrame:
+    return _default_ctx.from_batches(batches, schema=schema)
+
 
 __all__ = [
     "ExecutionContext",
@@ -13,4 +32,7 @@ __all__ = [
     "SchemaField",
     "ArrowColumn",
     "col",
+    "get_context",
+    "from_dict",
+    "from_batches",
 ]
--- mini-query-engine/mqe6/mqe/demo.py
+++ mini-query-engine/mqe6/mqe/demo.py
@@ -1,28 +1,28 @@
-from core import DataFrame, ExecutionContext, LazyFrame, col
+import core as mqe
+from core import col
 
 
 def main() -> None:
-    ct: ExecutionContext = ExecutionContext()
-
-    lf: LazyFrame = ct.from_dict(
-        {
-            "id": [1, 2, 3],
-            "first_name": ["Niko", "Alice", "Joy"],
-            "state": ["CO", "CA", "NY"],
-        }
+    # Example
+    lf: mqe.LazyFrame = (
+        mqe.from_dict(
+            {
+                "id": [1, 2, 3],
+                "first_name": ["Niko", "Alice", "Joy"],
+                "state": ["CO", "CA", "NY"],
+            }
+        )
+        .filter(col("first_name") == "Niko")
+        .select("id", (col("id") * 2).alias("new_id"), "first_name")
     )
 
-    lf = lf.filter(col("first_name") == "Niko").select(
-        "id", (col("id") * 2).alias("new_id"), "first_name"
-    )
-    
-    lf.explain(True)
+    lf.explain(verbose=True)
 
-    result: DataFrame = lf.collect()
+    result: mqe.DataFrame = lf.collect()
     print("\n===== EXAMPLE 1 =====\n")
     print(result)
 
-    result2: DataFrame = result.select("first_name")
+    result2: mqe.DataFrame = result.select("first_name")
     print("\n===== EXAMPLE 2 =====\n")
     print(result2)
 
